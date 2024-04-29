aab2fb5d80d174d45fe9e80119602bf16dc1c718
Pedro Vitor <82241565+PedrovSilva@users.noreply.github.com>
2024-04-29 19:02:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,16 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  let ipadrs = req.ip
+  let lang = req.headers["accept-language"]
+  let soft = req.headers["user-agent"]
+
+  res.json({
+    ipaddress: ipadrs,
+    language: lang,
+    software: soft
+  })
 });
 
 // listen for requests :)
